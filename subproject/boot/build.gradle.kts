c4a8afc96bfa6e5a8a7a5bb1a2d785802cfa4ae6
mango <mchead1@naver.com>
2025-03-20 19:06:19 +0900

    chore: jar configuration

--- subproject/boot/build.gradle.kts
+++ subproject/boot/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    kotlin("jvm")
     application
 }
 
@@ -6,9 +7,26 @@ dependencies {
     implementation(project(":subproject:application"))
     implementation(project(":subproject:domain"))
     implementation(project(":subproject:infrastructure"))
-    implementation(project(":subproject:presentation"))
 }
 
 application {
     mainClass.set("io.vending.machine.MainKt")
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "io.vending.machine.MainKt"
+    }
+
+    // Fat JAR 만들기 - 모든 의존성 포함
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath
+            .get()
+            .filter { it.name.endsWith("jar") }
+            .map { zipTree(it) }
+    })
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
